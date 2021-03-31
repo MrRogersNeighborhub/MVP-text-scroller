6444256b0487c208347f6df3f728c23d557a6a49
John Cooke <johncooke832@gmail.com>
2021-03-30 21:08:31 -0500

    Starting to figure it out!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,207 @@
-import React from 'react';
-// import storyTree from './story/storyTree.js';
+import React, { useState } from 'react';
+import Title from './pages/Title.jsx';
+// import StoryTree from './story/storyTree.js';
 
 const App = (props) => {
+  const story = {
+    1: {
+      text: 'Living with your father',
+      choices: [
+        {
+          text: 'Ask your father for words of wisdom',
+          next: 2,
+        },
+        {
+          text: 'Leave home for a life of asceticism',
+          next: 5,
+        }
+      ]
+    },
+    2: {
+      text: 'Your father tells you, "One dwells in suffering if one is without reverence and deference".',
+      choices: [
+        {
+          text: 'Ask your father for words of wisdom',
+          next: 3,
+        },
+        {
+          text: 'Leave home for a life of asceticism',
+          next: 5,
+        }
+      ]
+    },
+    3: {
+      text: 'Your father tells you, "A Brahmin once asked the Blessed One, "Are you a God?" "No, Brahmin," said the Blessed One. "Are you a saint?" "No, Brahmin," said the Blessed One. "Are you a magician?" "No, Brahmin," said the Blessed One. "What are you then?" "I am awake.""',
+      choices: [
+        {
+          text: 'Ask your father for words of wisdom',
+          next: 4,
+        },
+        {
+          text: 'Leave home for a life of asceticism',
+          next: 5,
+        }
+      ]
+    },
+    4: {
+      text: 'Your father tells you, "By whomsoever no evil is done in deed, or word, or thought, him I call a holy man who is guarded in these three."',
+      choices: [
+        {
+          text: 'Ask your father for words of wisdom',
+          next: 2,
+        },
+        {
+          text: 'Leave home for a life of asceticism',
+          next: 5,
+        }
+      ]
+    },
+    5: {
+      text: 'Living as a beggar',
+      choices: [
+        {
+          text: 'Go back home',
+          next: 1,
+        },
+        {
+          text: 'Accept a life of no worldly possessions',
+          next: 6,
+        }
+      ]
+    },
+    6: {
+      text: 'Learn of Gotama',
+      choices: [
+        {
+          text: 'You think to yourself, "there is no knowledge in that man that I cannot find within myself. I will renounce food until I find enlightenment."',
+          next: 7,
+        },
+        {
+          text: '"I will accompany you on this path."',
+          next: 8,
+        }
+      ]
+    },
+    7: {
+      text: 'Day 6 of your hunger strike, you are taking time to reflect through meditation. When you open your eyes again, you are laying on the ground, your head in Govinda\'s lap',
+      choices: [
+        {
+          text: '"I will join you in following Gotama"',
+          next: 8,
+        },
+        {
+          text: null
+        }
+      ]
+    },
+    8: {
+      text: 'Following Gotama',
+      choices: [
+        {
+          text: 'Seek answers threw meditation',
+          next: 9,
+        },
+        {
+          text: 'Tell Govinda that you still yern for something more.',
+          next: 10,
+        }
+      ]
+    },
+    9: {
+      text: 'After a month spent in deep meditation, you find you still have questions unanswered',
+      choices: [
+        {
+          text: 'Tell Govinda that you still yern for something more.',
+          next: 10,
+        }
+      ]
+    },
+    // 6: {
+    //   text: ,
+    //   choices: []
+    // },
+    // 7: {
+    //   text: ,
+    //   choices: []
+    // },
+    // 8: {
+    //   text: ,
+    //   choices: []
+    // },
+    // 9: {
+    //   text: ,
+    //   choices: []
+    // },
+    // 10: {
+    //   text: ,
+    //   choices: []
+    // },
+    // 11: {
+    //   text: ,
+    //   choices: []
+    // }
+    // 1: {
+    //   text: 'This is a test',
+    //   choices: [
+    //     {
+    //       text: 'Is this working?',
+    //       next: 2
+    //     },
+    //     {
+    //       text: 'This is working!',
+    //       next: 3
+    //     }
+    //   ]
+    // },
+    // 2: {
+    //   text: 'We did it!!',
+    //   choices: [
+    //     {
+    //       text: 'Is this working?',
+    //       next: 2
+    //     },
+    //     {
+    //       text: 'This is working!',
+    //       next: 3
+    //     }
+    //   ]
+    // },
+    // 3: {
+    //   text: 'We still did it!!',
+    //   choices: [
+    //     {
+    //       text: 'Is this working?',
+    //       next: 2
+    //     },
+    //     {
+    //       text: 'This is working!',
+    //       next: 3
+    //     }
+    //   ]
+    // }
+  }
+  const [showTitle, setPage] = useState(true);
+  const [currentPage, changePage] = useState(1);
 
+  const handleChangePage = (value) => {
+    changePage(value);
+  }
+
+  const continueStory = (e) => {
+    setPage(false);
+  }
 
   return (
     <div>
-
+      <div onClick={ continueStory }>
+        { showTitle ? <Title /> : <p>{ story[currentPage].text }</p> }
+        { showTitle ? null : <div onClick={ () => { handleChangePage(story[currentPage].choices[0].next) } }>
+          { story[currentPage].choices[0].text }
+        </div>}
+        { showTitle ? null : story[currentPage].choices[1].text? <div onClick={ () => { handleChangePage(story[currentPage].choices[1].next) } }>
+          { story[currentPage].choices[1].text }
+        </div> : null }
+      </div>
     </div>
   )
 }
--- src/story/storyTree.js
+++ src/story/storyTree.js
@@ -1,76 +1,83 @@
-class StoryTree {
-  constructor(page, value, next, altValue) {
-    this.page = page;
-    this.value = value;
-    this.altValue = altValue;
-    this.next =[];
-    this.nextPage = next;
-    this.count = 0;
-  }
-}
+// class StoryTree {
+//   constructor(page, value, next, altValue) {
+//     this.page = page;
+//     this.value = value;
+//     this.altValue = altValue;
+//     this.next =[];
+//     this.nextPage = next;
+//     this.count = 0;
+//   }
+// }
 
-const storyTree = new StoryTree(0, 'title');
+// const storyTree = new StoryTree(0, 'title');
 
-const pageOne = new StoryTree(1, 'Living with your father');
-storyTree.next.push(pageOne);
-const firstOptionforPageOne = new StoryTree(null, 'Gain knowledge from your father', 1);
-const secondOptionforPageOne = new StoryTree(null, 'Leave home for a life of asceticism', 2);
-pageOne.next.push(firstOptionforPageOne, secondOptionforPageOne);
+// const pageOne = new StoryTree(1, 'Living with your father');
+// storyTree.next.push(pageOne);
+// const firstOptionforPageOne = new StoryTree(null, 'Gain knowledge from your father', 1);
+// const secondOptionforPageOne = new StoryTree(null, 'Leave home for a life of asceticism', 2);
+// pageOne.next.push(firstOptionforPageOne, secondOptionforPageOne);
 
-const pageTwo = new StoryTree(2, 'Living as a beggar');
-secondOptionforPageOne.next.push(pageTwo);
-const firstOptionforPageTwo = new StoryTree(null, 'Go back home', 1);
-const secondOptionforPageTwo = new StoryTree(null, 'Accept a life of no worldly possessions', 3);
-pageTwo.next.push(firstOptionforPageTwo, secondOptionforPageTwo);
+// const pageTwo = new StoryTree(2, 'Living as a beggar');
+// secondOptionforPageOne.next.push(pageTwo);
+// const firstOptionforPageTwo = new StoryTree(null, 'Go back home', 1);
+// const secondOptionforPageTwo = new StoryTree(null, 'Accept a life of no worldly possessions', 3);
+// pageTwo.next.push(firstOptionforPageTwo, secondOptionforPageTwo);
 
-const pageThree = new StoryTree(3, 'Discover Gotama');
-secondOptionforPageTwo.next.push(pageThree);
-const firstOptionforPageThree = new StoryTree(null, 'Go on a hunger strike', 4, 'Stop living a life of asceticism and, instead, follow Gotama');
-pageThree.next.push(firstOptionforPageThree);
+// const pageThree = new StoryTree(3, 'Discover Gotama');
+// secondOptionforPageTwo.next.push(pageThree);
+// const firstOptionforPageThree = new StoryTree(null, 'Go on a hunger strike', 4, 'Stop living a life of asceticism and, instead, follow Gotama');
+// pageThree.next.push(firstOptionforPageThree);
 
-const pageFour = new StoryTree(4, 'Following Gotama');
-firstOptionforPageThree.next.push(pageFour);
-const firstOptionforPageFour = new StoryTree(null, 'Seek answers through meditation', 4);
-const secondOptionforPageFour = new StoryTree(null, 'Leave your life following Gotama with your friend Govinda and take a ferry to the city to pursue a life of pleasure', 5);
-pageFour.next.push(firstOptionforPageFour, secondOptionforPageFour);
+// const pageFour = new StoryTree(4, 'Following Gotama');
+// firstOptionforPageThree.next.push(pageFour);
+// const firstOptionforPageFour = new StoryTree(null, 'Seek answers through meditation', 4);
+// const secondOptionforPageFour = new StoryTree(null, 'Leave your life following Gotama with your friend Govinda and take a ferry to the city to pursue a life of pleasure', 5);
+// pageFour.next.push(firstOptionforPageFour, secondOptionforPageFour);
 
-const pageFive = new StoryTree(5, 'Starting a life of physical please');
-const firstOptionforPageFive = new StoryTree(null, 'Realize there can be no enlightenment in this life and return to your friend', 4);
-const secondOptionforPageFive = new StoryTree(null, 'Start a business as a merchant');
-pageFive.next.push(firstOptionforPageFive, secondOptionforPageFive);
+// const pageFive = new StoryTree(5, 'Starting a life of physical please');
+// secondOptionforPageFour.next.push(pageFive);
+// const firstOptionforPageFive = new StoryTree(null, 'Realize there can be no enlightenment in this life and return to your friend', 4);
+// const secondOptionforPageFive = new StoryTree(null, 'Start a business as a merchant');
+// pageFive.next.push(firstOptionforPageFive, secondOptionforPageFive);
 
-const pageSix = new StoryTree(6, 'Embracing a life of pleasure');
-const firstOptionforPageSix = new StoryTree(null, 'Find an opium den', /* Ending one, death in an opium den */);
-const secondOptionforPageSix = new StoryTree(null, 'Leave in the middle of the night', 7);
-pageSix.next.push(firstOptionforPageSix, secondOptionforPageSix);
+// const pageSix = new StoryTree(6, 'Embracing a life of pleasure');
+// secondOptionforPageFive.next.push(pageSix);
+// const firstOptionforPageSix = new StoryTree(null, 'Find an opium den', /* Ending one, death in an opium den */);
+// const secondOptionforPageSix = new StoryTree(null, 'Leave in the middle of the night', 7);
+// pageSix.next.push(firstOptionforPageSix, secondOptionforPageSix);
 
-const pageSeven = new StoryTree(7, 'Awaken by the river');
-const firstOptionforPageSeven = new StoryTree(null, 'Join your friend', /* Ending two, spend the rest of your life looking for nirvana with your friend */);
-const secondOptionforPageSeven = new StoryTree(null, '', );
-pageSeven.next.push(firstOptionforPageSeven, secondOptionforPageSeven);
+// const pageSeven = new StoryTree(7, 'Awaken by the river');
+// secondOptionforPageSix.next.push(pageSeven);
+// const firstOptionforPageSeven = new StoryTree(null, 'Join your friend', /* Ending two, spend the rest of your life looking for nirvana with your friend */);
+// const secondOptionforPageSeven = new StoryTree(null, 'Go back to the ferryman', 8);
+// pageSeven.next.push(firstOptionforPageSeven, secondOptionforPageSeven);
 
-// const page = new StoryTree();
-// const firstOptionforPage = new StoryTree(null, '', );
-// const secondOptionforPage = new StoryTree(null, '', );
-// page.next.push(firstOptionforPage, secondOptionforPage);
+// const pageEight = new StoryTree(8, 'Reconvening with the ferryman');
+// secondOptionforPageSeven.next.push(pageEight);
+// const firstOptionforPageEight = new StoryTree(null, 'Ask for the secret to happiness', 8);
+// const secondOptionforPageEight = new StoryTree(null, 'Ask for work', 9);
+// pageEight.next.push(firstOptionforPageEight, secondOptionforPageEight);
 
-// const page = new StoryTree();
-// const firstOptionforPage = new StoryTree(null, '', );
-// const secondOptionforPage = new StoryTree(null, '', );
-// page.next.push(firstOptionforPage, secondOptionforPage);
+// const pageNine = new StoryTree(9, 'Reuniting with Kamala');
+// secondOptionforPageEight.next.push(pageNine);
+// const firstOptionforPageNine = new StoryTree(null, 'Take her son back to the city', /* Ending three, you raise your son, but he eventually leaves you */);
+// const secondOptionforPageNine = new StoryTree(null, 'Raise your son by the river', 10);
+// pageNine.next.push(firstOptionforPageNine, secondOptionforPageNine);
 
-// const page = new StoryTree();
-// const firstOptionforPage = new StoryTree(null, '', );
-// const secondOptionforPage = new StoryTree(null, '', );
-// page.next.push(firstOptionforPage, secondOptionforPage);
+// const pageTen = new StoryTree(10, 'Your son steals your belongings and leaves in the night');
+// secondOptionforPageNine.next.push(pageTen);
+// const firstOptionforPageTen = new StoryTree(null, 'You can never stop searching for your son', /* Ending three, you find and raise your son, but he eventually leaves you */);
+// const secondOptionforPageTen = new StoryTree(null, 'You realize that chasing your son would be futile, so you stay by with the river', 11);
+// pageTen.next.push(firstOptionforPageTen, secondOptionforPageTen);
 
-// const page = new StoryTree();
-// const firstOptionforPage = new StoryTree(null, '', );
-// const secondOptionforPage = new StoryTree(null, '', );
-// page.next.push(firstOptionforPage, secondOptionforPage);
+// const pageEleven = new StoryTree(11, 'Living as a ferryman');
+// secondOptionforPageTen.next.push(pageEleven);
+// const firstOptionforPageEleven = new StoryTree(null, 'Tell your friend the secret of nirvana');
+// const secondOptionforPageEleven = new StoryTree(null, 'Tell your friend to kiss your forehead');
+// pageEleven.next.push(firstOptionforPageEleven, secondOptionforPageEleven);
 
 
 
-module.export = {
-  storyTree,
-}
+// module.export = {
+//   storyTree,
+// }
